58513735d21e8d4a065d11e7f23f769ea90140e0
Anton <agadjanyan.anton@yandex.ru>
2024-06-11 22:31:58 +1000

    test 2 edit

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 
-def about(request):
+def about(request: str):
     return render(request, 'pages/about.html')
 
 
-def rules(request):
+def rules(request: str):
     return render(request, 'pages/rules.html')
