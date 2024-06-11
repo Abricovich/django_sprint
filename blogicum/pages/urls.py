b107b67886cdde027ee71b54f748eb12c3d83c60
Anton <agadjanyan.anton@yandex.ru>
2024-06-11 22:24:14 +1000

    test edit

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -4,8 +4,8 @@ from .views import about, rules
 
 app_name = 'pages'
 
-# Здесь будут три адреса - второй с детальной информацией публиукации, третий будет с категориями
+
 urlpatterns = [
     path('about/', about, name='about'),
-    path('rules/', rules, name='rules') 
+    path('rules/', rules, name='rules')
 ]
